635b5f0bafa28fccc4b4613788bb7443faa3e10b
Luis Henrique Arandas <luis.arandas@gmail.com>
2018-11-11 16:59:25 +0000

    oscilator broadcast

--- public/sketch.js
+++ public/sketch.js
@@ -1,16 +1,20 @@
 var socket;
+var osc;
 
 function setup() {
+
   createCanvas(windowWidth, windowHeight);
   background(51);
-
   socket = io.connect(window.location.origin);
   socket.on('mouse', newDrawing);
+
+  osc = new p5.TriOsc();
+  osc.amp(0.5);
+  osc.start();
+
 }
 
-function draw() {
-  ellipse(width / 2, height / 2, 200, 200);
-}
+function draw() {}
 
 function mouseDragged() {
 
@@ -26,11 +30,15 @@ function mouseDragged() {
   noStroke();
   fill(255);
   ellipse(mouseX, mouseY, 50, 50);
-
 }
 
 function newDrawing(data) {
   noStroke();
   fill(255, 0, 0);
   ellipse(data.x, data.y, 50, 50);
+  //teste a enviar valores para o som
+  var freq = map(data.x, 0, width, 40, 880);
+  osc.freq(freq);
+  var amp = map(data.y, 0, height, 1, .01);
+  osc.amp(amp);
 }
