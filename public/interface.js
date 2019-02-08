0d36f8ce8cd349fb361f25f896ab1dc706b2a14c
Luis Henrique Arandas <luis.arandas@gmail.com>
2019-02-08 17:39:02 +0000

    changed minim files need nxt version

--- public/interface.js
+++ public/interface.js
@@ -1,42 +1,29 @@
 window.WUI_Reporting = true;
 
-//allow console.log reporting
-
 var master_dialog = WUI_Dialog.create("master_dialog", {
   title: "Master Controls",
   width: "275px",
   height: "400px",
   halign: "left",
   valign: "center",
-  open: true, // wether the dialog is opened or not after creation
-  minimized: false, // wether the dialog is minimized or not after creation
-  on_open: null, // function called when the dialog has been opened
-  on_close: null, // function called when the dialog has been closed
-  on_pre_detach: function() {}, // function called before the dialog is detached
-  on_detach: function(new_window) { // function called when the dialog is detached, the new `window` object is passed as argument
-    new_window.document.title = "Master"; // replace the detached dialog title
+  open: true,
+  minimized: false,
+  on_open: null,
+  on_close: null,
+  on_pre_detach: function() {},
+  on_detach: function(new_window) {
+    new_window.document.title = "Master";
     bind_contextmenu();
   },
-  // function called when the dialog is resized, `new_width` and `new_height` is the new dimension of the dialog content
   on_resize: function(new_width, new_height) {},
-  // you can add header buttons easily by using this (such as a help "?" button)
-  header_btn: [{
-    title: "Help",
-    on_click: function() {
-      // code called when the header button is clicked, redirecting to a documentation for example
-    },
-    class_name: "" // class name to use (you will generally need it if you want to show an icon like "?", use the background-image property to do so)
-  }],
   modal: false,
   closable: false,
   draggable: true,
   minimizable: true,
   resizable: false,
   detachable: true,
-  min_width: "title",
-  min_height: 64,
   keep_align_when_resized: false,
-  top: 10,
+  top: -250,
   left: 0
 });
 
@@ -44,47 +31,33 @@ var master_dialog = WUI_Dialog.create("master_dialog", {
 var cockpit_dialog = WUI_Dialog.create("cockpit_dialog", {
   title: "Instruments", //'<div style="font-family: Monospace; font-size: 10px; color: lightgrey; position: absolute; margin-left: 8px;">---</div><span style="font-family: Monospace; font-size: 10px; color: lightgrey;">Instruments</span>',
   width: "850px",
-  height: "35%",
+  height: "280px",
   halign: "center",
   valign: "bottom",
-  open: true, // wether the dialog is opened or not after creation
-  minimized: false, // wether the dialog is minimized or not after creation
-  on_open: null, // function called when the dialog has been opened
-  on_close: null, // function called when the dialog has been closed
-  on_pre_detach: function() {}, // function called before the dialog is detached
-  // function called when the dialog is detached, the new `window` object is passed as argument
+  open: true,
+  minimized: false,
+  on_open: null,
+  on_close: null,
+  on_pre_detach: function() {},
   on_detach: function(new_window) {
-    // you can modify the detachable window there, example:
     new_window.document.title = "Instruments and Controls"; // replace the detached dialog title
   },
-  // function called when the dialog is resized, `new_width` and `new_height` is the new dimension of the dialog content
   on_resize: function(new_width, new_height) {},
-  // you can add header buttons easily by using this (such as a help "?" button)
   header_btn: [{
     title: "?",
     on_click: function() {
       console.log("BOTON");
     },
-    class_name: "" // class name to use (you will generally need it if you want to show an icon like "?", use the background-image property to do so)
+    class_name: ""
   }],
   modal: false,
-
-  // wether the dialog have a status bar
-  //status_bar: true,
-  // HTML content of the status bar
-  //status_bar_content: "status bar content",
-
   closable: false,
   draggable: true,
   minimizable: true,
-  //resizable: true,
   detachable: true,
-  // the minimun width/height the dialog can be when resized (min_width accept a value or "title")
   min_width: "title",
   min_height: 64,
-  // option to keep the align when resized, example: if the dialog is centered in the window, the dialog will always be in center when it is resized
   keep_align_when_resized: true,
-  // can be used to position the dialog, default to 0
   top: 0,
   left: 0,
 });
@@ -93,7 +66,6 @@ WUI_Tabs.create("my_tabs", {
   // function called when a tab is clicked (tab index will be passed as argument)
   on_tab_click: console.log("ok"),
   // style value for the content height
-  height: "100%" // this is the default value
 });
 
 
@@ -120,12 +92,19 @@ WUI_Dialog.create("demo_integrated_dialog", {
   valign: "top",
   closable: false,
   draggable: true,
-  minimizable: true,
+  //minimizable: true,
   status_bar: false,
   width: "195px",
   height: "195px",
   top: 3,
-  left: -325
+  left: -325,
+  header_btn: [{
+    title: "?",
+    on_click: function() {
+      console.log("BOTON");
+    },
+    class_name: ""
+  }],
 });
 
 WUI_Dialog.create("demo_integrated_dialog_2", {
@@ -134,12 +113,19 @@ WUI_Dialog.create("demo_integrated_dialog_2", {
   valign: "top",
   closable: false,
   draggable: true,
-  minimizable: true,
+  //minimizable: true,
   status_bar: false,
   width: "195px",
   height: "195px",
   top: -192,
-  left: -125
+  left: -125,
+  header_btn: [{
+    title: "?",
+    on_click: function() {
+      console.log("BOTON");
+    },
+    class_name: ""
+  }],
 });
 
 WUI_Dialog.create("demo_integrated_dialog_3", {
@@ -148,29 +134,17 @@ WUI_Dialog.create("demo_integrated_dialog_3", {
   valign: "top",
   closable: false,
   draggable: true,
-  minimizable: true,
+  //minimizable: true,
   status_bar: false,
   width: "195px",
   height: "195px",
   top: -387,
-  left: 75
+  left: 75,
+  header_btn: [{
+    title: "?",
+    on_click: function() {
+      console.log("BOTON");
+    },
+    class_name: ""
+  }],
 });
-
-
-function bind_contextmenu(win) {
-  console.log("ok");
-  win.document.addEventListener("contextmenu", function(ev) {
-    ev.preventDefault();
-
-    WUI_CircularMenu.create({
-      x: ev.pageX,
-      y: ev.pageY,
-
-      rx: 34,
-      ry: 34,
-
-      window: win
-    }, circular_menu_items);
-  });
-}
-bind_contextmenu(window);
